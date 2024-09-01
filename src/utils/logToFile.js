20576872cf89b636d8075b657bac89f67e313138
Luciano Guida <luciano.guida@plenaalimentos.com.br>
2024-09-01 12:39:49 -0300

    formatação do arquivo de log

--- src/utils/logToFile.js
+++ src/utils/logToFile.js
@@ -3,6 +3,18 @@ const path = require('path');
 
 const logsDir = path.join(__dirname, '../../logs');
 
+function formatDateTime() {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Mês é baseado em zero
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+}
+
 module.exports = (message) => {
     const date = new Date().toISOString().split('T')[0]; // Formato: YYYY-MM-DD
     const logFileName = `${date}.log`;
@@ -14,6 +26,6 @@ module.exports = (message) => {
     }
 
     // Adiciona a mensagem ao arquivo de log
-    fs.appendFileSync(logFilePath, `${new Date().toISOString()} - ${message}\n`);
+    fs.appendFileSync(logFilePath, `${formatDateTime()} - ${message}\n`);
     console.log(message);
 }
