cf40a2607a47f022cb5548e237317e6b8d81a2e7
Luciano Guida <luciano.guida@plenaalimentos.com.br>
2024-09-03 22:02:32 -0300

    teste-vercel

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ routes(app);
 
 const request = new RequestController();
 
+request.dadosSport()
 // Configurando a tarefa cron para executar a cada 5 horas
 cron.schedule('0 */5 * * *', async () => {
     await request.dadosSport();
@@ -18,7 +19,6 @@ cron.schedule('0 */3 * * *', async () => {
 });
 
 app.get('/teste',async (req, res) => {
-    console.log(await request.dadosSport());
     res.status(200).send({mensagem: 'Ok!'})
 })
 
