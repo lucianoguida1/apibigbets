cc2f8c33df49f292f36ad369d8193a8ee204dd1b
Luciano Guida <luciano.guida@plenaalimentos.com.br>
2024-09-06 15:40:04 -0300

    configuração do pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,10 @@
 module.exports = {
     apps: [
         {
-            name: 'apidash',
+            name: 'api-bigbets',
             script: 'server.js',
+            instances: 1, // Executar apenas uma instância
+            exec_mode: 'fork', // Usar modo fork para uma única instância
             watch: true, // Ativa o monitoramento de alterações
             ignore_watch: [
                 "src/database/storage/",
@@ -10,9 +12,16 @@ module.exports = {
                 "logs/",
                 "dist/"
             ],
+            max_memory_restart: '500M', // Reiniciar se usar mais de 300MB de memória
+            autorestart: true, // Reiniciar automaticamente em caso de falha
+            max_restarts: 5, // Limite de reinicializações consecutivas
+            restart_delay: 5000, // Aguardar 5 segundos antes de reiniciar
             env: {
                 NODE_ENV: 'production',
             },
+            env_development: {
+                NODE_ENV: 'development',
+            }
         },
     ],
 };
