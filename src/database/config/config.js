3c15fdb0f463c94048a6d9a64cdfd4198a019cd2
Luciano Guida <luciano.guida@plenaalimentos.com.br>
2024-10-01 12:07:45 -0300

    volta ao download db

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,9 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "dialect": "sqlite",
-    "storage": "./src/database/storage/database.db",
-    "logging": false
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": "postgres",
+    "dialectModule": require('pg'),
+    "logging": false,
   },
   "test": {
     "username": "root",
--- src/routes/index.js
+++ src/routes/index.js
@@ -9,6 +9,7 @@ const servicesBase = require('./services.js')
 const requestRouter = require('./request.js')
 const estrategia = require('./estrategia.js')
 const regra = require('./regra.js')
+const path = require('path');
 
 const serviceBase = new ServicesBaseController();
 const request = new RequestController();
@@ -23,6 +24,18 @@ module.exports = app => {
         estrategia,
         regra,
     );
+
+
+    const dbFilePath = path.join(__dirname, '../database/storage/bigbets.backup');
+    app.get('/downloaddb', (req, res) => {
+        res.download(dbFilePath, 'database.db', (err) => {
+            if (err) {
+                console.error('Erro ao fazer download da base de dados:', err);
+                res.status(500).send('Erro ao baixar o banco de dados');
+            }
+        });
+    });
+
 
     app.get('/executa/:data?', async (req, res) => {
         const data = req.params.data;
