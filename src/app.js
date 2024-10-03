3f788e80bb6ac595d76054a01f81523b7c31e7d2
Luciano Guida <luciano.guida@plenaalimentos.com.br>
2024-10-03 09:13:22 -0300

    limpa banco em massa

--- src/app.js
+++ src/app.js
@@ -6,38 +6,31 @@ const app = express();
 routes(app);
 
 
-const pLimit = require('p-limit');
-const limit = pLimit(5000); // Limita a 100 exclusões simultâneas
-
+const RegraSev = require('./services/RegravalidacoeServices.js');
+const regraserv = new RegraSev();
 async function processarRegras() {
     try {
         console.time('Tempo de Execução'); // Inicia a contagem do tempo
 
         const regras = await regraserv.pegaTodosOsRegistros();
 
-        // Limita o número de promessas simultâneas
-        const promessas = regras.map((regra) =>
-            limit(async () => {
-                const odds = await regra.getOdds();
-                if (odds.length === 0) {
-                    console.log('regra: ' + regra.id);
-                    await regra.destroy({ force: true }); // Exclui a regra permanentemente
-                }
-            })
-        );
-
-        // Espera todas as promessas serem resolvidas
-        await Promise.all(promessas);
+        for (const regra of regras) {
+            const odds = await regra.getOdds(); // Pega as odds associadas à regra
+            if (odds.length === 0) {
+                console.log('regra: ' + regra.id)
+                await regra.destroy({ force: true }); // Exclui a regra se não tiver odds associadas
+            }
+        }
 
         console.timeEnd('Tempo de Execução'); // Exibe o tempo de execução no console
     } catch (error) {
         console.error("Erro ao buscar ou processar as regras:", error);
     }
-    console.log('passou todos');
+    console.log('passou todos')
 }
 
+// Chama a função assíncrona
 processarRegras();
-
 
 
 // EXECUTA AS TAREFAS CRONS
