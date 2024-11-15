b4cb4465d2d25cc9aac563465435da2e439423a9
luciano Guida <lucianoguida10@gmail.com>
2024-11-15 15:03:50 -0300

    adição da rota de testar estrategias

--- src/routes/estrategia.js
+++ src/routes/estrategia.js
@@ -12,7 +12,7 @@ router.get('/estrategia/bilhetes/:id', (req, res) => estrategiaController.getBil
 router.get('/estrategia/executa/:id', (req, res) => estrategiaController.executarEstrategia(req, res));
 router.post('/estrategia', (req, res) => estrategiaController.criarEstrategia(req, res));
 router.post('/estrategia/teste/', (req, res) => estrategiaController.estrategiaTeste(req, res));
-router.put('/estrategia/:id', (req, res) => estrategiaController.atualizarEstrategia(req, res));
+//router.put('/estrategia/:id', (req, res) => estrategiaController.atualizarEstrategia(req, res));
 router.delete('/estrategia/:id', (req, res) => estrategiaController.exclui(req, res));
 router.get('/topestrategia', (req, res) => estrategiaController.getTopEstrategia(req, res));
 
