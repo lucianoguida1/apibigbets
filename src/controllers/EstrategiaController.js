0c47604190bbbfeb9f89e8a49a2151334a70d306
luciano Guida <lucianoguida10@gmail.com>
2024-11-15 23:37:40 -0300

    executa estrategia

--- src/controllers/EstrategiaController.js
+++ src/controllers/EstrategiaController.js
@@ -567,7 +567,7 @@ class EstrategiaController extends Controller {
             if (!estrategia) {
                 return res.status(404).json({ error: 'Estratégia não encontrada!' });
             }
-            await bilheteServices.montaBilhetes(estrategia);
+            await bilheteServices.montaBilhetes(estrategia, true);
             await estrategiaServices.geraEstistica(estrategia);
             const estrategiaA = await estrategiaServices.pegaUmRegistroPorId(id);
             return res.status(200).json({ message: 'Estratégia atualizada com sucesso!', estrategia: estrategiaA });
