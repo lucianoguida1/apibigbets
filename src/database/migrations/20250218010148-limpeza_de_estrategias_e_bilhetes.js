274b30747285f902db3ae1df7cfa9d520b619160
Luciano Guida <lucianoguida10@gmail.com>
2025-02-25 21:57:45 -0300

    adiciona limpeza de dados na migração para atualizar o status das odds e remover bilhetes e estratégias

--- src/database/migrations/20250218010148-limpeza_de_estrategias_e_bilhetes.js
+++ src/database/migrations/20250218010148-limpeza_de_estrategias_e_bilhetes.js
@@ -5,5 +5,6 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkDelete('bilhetes', null, {});
     await queryInterface.bulkDelete('estrategias', null, {});
+    await queryInterface.bulkUpdate('odds', { status: null }, {});
   }
 };
