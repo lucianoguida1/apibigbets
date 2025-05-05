1efb0899072bb0345b12fe3e8775014cd06879c8
Luciano Guida <lucianoguida10@gmail.com>
2025-05-04 22:53:45 -0300

    refactor: remove configuração de caminho base do Bull Board e simplifica a inicialização do router

--- src/app.js
+++ src/app.js
@@ -42,19 +42,16 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 
 
-const bullBoardPath = '/api1.0/adminwl/queues';
 
-const { router: bullBoardRouter } = createBullBoard({
-  queues: Queue.queues.map(queue => new BullAdapter(queue.bull)),
-  options: {
-    basePath: bullBoardPath, // <- Define corretamente o caminho base
-  },
-});
+const { router: bullBoardRouter } = createBullBoard(
+  Queue.queues.map(queue => new BullAdapter(queue.bull))
+);
 
 // Corrige o caminho interno dos assets da UI do Bull Board
 app.use('/adminwl/queues', (req, res, next) => {
   // Faz o Bull Board funcionar mesmo com prefixo do NGINX
   req.originalUrl = req.baseUrl + req.url;
+  console.log('req.baseUrl + req.url', req.baseUrl + req.url)
   return bullBoardRouter(req, res, next);
 });
 
